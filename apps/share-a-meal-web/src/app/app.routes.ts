662d089d0fd254a59fed387eeca9a5376e0ce9aa
Daan de Vries <jumpsuitfrost@gmail.com>
2023-11-20 10:09:44 +0100

    Added deployment

--- apps/share-a-meal-web/src/app/app.routes.ts
+++ apps/share-a-meal-web/src/app/app.routes.ts
@@ -1,45 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UserListComponent } from 'libs/share-a-meal/features/src/lib/user/user-list/user-list.component';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-import { UserService } from 'libs/share-a-meal/features/src/lib/user/user.service';
-import { UserDetailComponent } from 'libs/share-a-meal/features/src/lib/user/user-detail/user-detail.component';
-import { UserEditComponent } from 'libs/share-a-meal/features/src/lib/user/user-edit/user-edit.component';
+
 
 
 import { FormsModule } from '@angular/forms';
 
 export const appRoutes: Routes = [
   {
-    path: 'new',
-    pathMatch: 'full',
-    component: UserEditComponent,
+    path: '',
+    loadChildren: () =>
+    import ('@avans-nx-workshop/share-a-meal/features').then(
+      (module) => module.FeaturesModule
+    ),
   },
+
   {
-    path: ':id/edit',
+    path: '**',
+    redirectTo: '/about',
     pathMatch: 'full',
-    component: UserEditComponent,
   },
-  {
-    path: ':id',
-    pathMatch: 'full',
-    component: UserDetailComponent,
-  },
-  {
-    path: '', 
-    pathMatch: 'full',
-    component: UserListComponent,
-  },
+
 ];
-
-
-
-@NgModule({
-  imports: [RouterModule.forChild(appRoutes), CommonModule, HttpClientModule, FormsModule],
-  //declarations: [UserListComponent, UserDetailComponent, UserEditComponent],
-  providers: [UserService],
-  exports: [RouterModule],
-})
-export class UserModule {}
-
--- libs/share-a-meal/features/src/lib/features.module.ts
+++ libs/share-a-meal/features/src/lib/features.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router'; // Import RouterModule
+import { RouterModule, Routes } from '@angular/router'; // Import RouterModule
 import { MealListComponent } from './meal/meal-list/meal-list.component';
 import { MealDetailComponent } from './meal/meal-detail/meal-detail.component';
 import { MealService } from './meal/meal.service';
@@ -10,8 +10,31 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserService } from './user/user.service';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 
+const appRoutes: Routes = [
+  {
+    path: 'user/new',
+    pathMatch: 'full',
+    component: UserEditComponent,
+  },
+  {
+    path: 'user/:id/edit',
+    pathMatch: 'full',
+    component: UserEditComponent,
+  },
+  {
+    path: 'user/:id',
+    pathMatch: 'full',
+    component: UserDetailComponent,
+  },
+  {
+    path: 'user',
+    pathMatch: 'full',
+    component: UserListComponent,
+  },
+];
+
 @NgModule({
-  imports: [CommonModule, HttpClientModule, RouterModule], // Include RouterModule here
+  imports: [CommonModule, HttpClientModule, RouterModule.forChild(appRoutes)], // Include RouterModule here
   declarations: [
     MealListComponent,
     MealDetailComponent,
@@ -29,4 +52,5 @@ import { UserEditComponent } from './user/user-edit/user-edit.component';
   ],
 })
 export class FeaturesModule {}
+
 
